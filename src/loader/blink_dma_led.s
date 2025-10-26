3f0a40d700ab4b6388914cf24fd537bb001a69a6
Joseph Freeston <freestoj@lafayette.edu>
2025-10-25 20:53:10 -0400

    Working dma_access blinky 6502 program

--- src/loader/blink_dma_led.s
+++ src/loader/blink_dma_led.s
@@ -3,6 +3,9 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;
 ; This code is an example that can be loaded to the 65c02
+; It requires no low-mem, stack, etc.
+;
+; The DMA_ACCESS LED should blink once, twice, thrice, four times, and the cycle will repeat.
 ;
 ; Compile & run with
 ; ca65 blink_dma_led.s -o blink_dma_led.o && ld65 -C loader6502.cfg -o blink_dma_led blink_dma_led.o && sudo python ../programmer/load_dma_and_run.py ./blink_dma_led
@@ -31,9 +34,24 @@ entry:
 loop:
     LDA $00
     NOP
+
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+
+    LDA $00
     NOP
     LDA $00
-    STA $01
+    NOP
+
+
     NOP
     NOP
     NOP
@@ -42,7 +60,49 @@ loop:
     NOP
     NOP
     NOP
-    
+    NOP
+    NOP
+
+    LDA $00
+    NOP
+    LDA $00
+    NOP
+    LDA $00
+    NOP
+
+
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+
+    LDA $00
+    NOP
+    LDA $00
+    NOP
+    LDA $00
+    NOP
+    LDA $00
+    NOP
+
+
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+
     jmp loop
     ; BRA loop
 
